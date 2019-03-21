43ec0ac2f7c4f1477fbe6c94b8c4cb4f222a7278
Henk Tjalsma <G00376321@gmit.ie>
2019-03-21 19:50:58 +0000

    Changed faithful.py script, 210319:19:50

--- faithful.py
+++ faithful.py
@@ -5,10 +5,17 @@
 import numpy
 
 # Read data file into array. Ensure columns headers are removed.
+# https://stackoverflow.com/questions/3518778/how-do-i-read-csv-data-into-a-record-array-in-numpy
+# from numpy import genfromtxt
+# my_data = genfromtxt('my_file.csv', delimiter=',')
+
 data = numpy.genfromtxt('oldfaithful.csv', delimiter=',')
 
+# : for everything in the 1st dimension & ,0 meaning in the 2nd dimension give me the first thing
 firstcol = data[:,0]
 meanfirstcol = numpy.mean(data[:,0])
+
+# secondcol = data[:,1]
 
 print("Average of first column is: ", meanfirstcol)
 
